2f466701af2f78c0c2e0296e329f94c19c71c538
Alex Leung <leungxa@gmail.com>
2013-06-02 12:25:04 -0700

    modified ship shapes

--- public/javascripts/ship.js
+++ public/javascripts/ship.js
@@ -3,29 +3,55 @@
 	Pyramid implementation adapted from learningwebgl.com
 */
 
+var size = 20;
+
 function Ship() {
 	this.posBuffer;
 	this.colorBuffer;
 
 	this.vertices = [
 		// Front face
-		 0.0,  1.0,  0.0,
-		-1.0, -1.0,  1.0,
-		 1.0, -1.0,  1.0,
+		 0.0,  0.0,  3.0,
+		-1.0, -1.0, -1.0,
+		 1.0, -1.0, -1.0,
 		// Right face
-		 0.0,  1.0,  0.0,
-		 1.0, -1.0,  1.0,
+		 0.0,  0.0,  3.0,
 		 1.0, -1.0, -1.0,
+		 4.0,  0.0,  0.0,
+		//
+		 0.0,  0.0,  3.0,
+		 4.0,  0.0,  0.0,
+		 1.0,  1.0, -1.0,
 		// Back face
-		 0.0,  1.0,  0.0,
-		 1.0, -1.0, -1.0,
-		-1.0, -1.0, -1.0,
+		 0.0,  0.0,  3.0,
+		 1.0,  1.0, -1.0,
+		-1.0,  1.0, -1.0,
 		// Left face
-		 0.0,  1.0,  0.0,
+		 0.0,  0.0,  3.0,
+		-1.0,  1.0, -1.0,
+		-4.0,  0.0,  0.0,		
+		 // 
+		 0.0,  0.0,  3.0,
+		-4.0,  0.0,  0.0,
 		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0
+		// Bottom Right
+		 4.0,  0.0,  0.0,
+		 1.0, -1.0, -1.0,
+		 1.0, -1.0, -1.0,
+		// Bottom Left
+		-4.0,  0.0,  0.0,
+		-1.0,  1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		// Bottom
+		 1.0, -1.0, -1.0,
+		 1.0,  1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		//
+		-1.0, -1.0, -1.0,
+		-1.0,  1.0, -1.0,
+		 1.0, -1.0, -1.0
 	];
-	this.vertices = this.vertices.map(function(x) { return x * 20; });
+	this.vertices = this.vertices.map(function(x) { return x * size; });
 
 	this.colors = [
 		// Front face
@@ -43,6 +69,30 @@ function Ship() {
 		// Left face
 		1.0, 0.0, 0.0, 1.0,
 		0.0, 0.0, 1.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		// Left face
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		// Left face
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		// Back face
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		// Left face
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		// Left face
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		// Left face
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
 		0.0, 1.0, 0.0, 1.0
 	];
 }
@@ -52,16 +102,16 @@ Ship.prototype.initBuffers = function(gl, shaderProgram) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
 	this.posBuffer.itemSize = 3;
-	this.posBuffer.numItems = 12;
+	this.posBuffer.numItems = 30;
 
 	this.colorBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.STATIC_DRAW);
 	this.colorBuffer.itemSize = 4;
-	this.colorBuffer.numItems = 12;
+	this.colorBuffer.numItems = 30;
 }
 
-Ship.prototype.draw = function(gl, shaderProgram) {
+Ship.prototype.draw = function(gl, shaderProgram, frag) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.posBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -69,16 +119,4 @@ Ship.prototype.draw = function(gl, shaderProgram) {
 	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, this.colorBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
 	gl.drawArrays(gl.TRIANGLES, 0, this.posBuffer.numItems);
-}
-
-Ship.prototype.createPlayers = function(players) {
-	var playersArray = [];
-	for (var i = 0; i < players.length; i++) {
-		var playerMatrix = mat4.create();
-		mat4.identity(playerMatrix);
-		mat4.translate(playerMatrix, playerMatrix, players[i]);
-		playersArray.push(playerMatrix);
-	}
-	console.log(playersArray[1]);
-	return playersArray;
 }
